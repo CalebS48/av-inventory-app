efb6d578019749bdc4bb69e1fca1ab28832bad27
CalebS48 <161987636+CalebS48@users.noreply.github.com>
2025-10-17 14:27:00 -0400

    Update and rename tesst.html to App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AV Inventory Management System</h1>
+        <p>Frontend code is now active.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
